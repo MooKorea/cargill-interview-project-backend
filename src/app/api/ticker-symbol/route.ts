111ab833d8358aa85fae8d69f915ff20e643e5b2
MooKorea <69235304+MooKorea@users.noreply.github.com>
2024-07-29 18:50:56 -0500

    update cache 2

--- src/app/api/ticker-symbol/route.ts
+++ src/app/api/ticker-symbol/route.ts
@@ -1,3 +1,5 @@
+const cache = new Map();
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const query = searchParams.get("query");
@@ -6,9 +8,24 @@ export async function GET(request: Request) {
     return new Response("Error: query not provided", { status: 400 });
   }
 
-  const url = `https://financialmodelingprep.com/api/v3/search?query=${query.toUpperCase()}&limit=10&apikey=${
-    process.env.FINANCIAL_MODELING_PREP_KEY
-  }`;
+  const cacheKey = query.toUpperCase();
+  const cachedResponse = cache.get(cacheKey);
+
+  if (cachedResponse) {
+    return new Response(JSON.stringify(cachedResponse), {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Cache-Control": "public, s-maxage=1800",
+        "CDN-Cache-Control": "public, s-maxage=1800",
+        "Vercel-CDN-Cache-Control": "public, s-maxage=3600",
+      },
+    });
+  }
+
+  const url = `https://financialmodelingprep.com/api/v3/search?query=${cacheKey}&limit=10&apikey=${process.env.FINANCIAL_MODELING_PREP_KEY}`;
 
   try {
     const res = await fetch(url, {
@@ -18,21 +35,21 @@ export async function GET(request: Request) {
     });
     const data = await res.json();
 
-    return Response.json(
-      { data },
-      {
-        status: 200,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET",
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
-          "Cache-Control": "public, s-maxage=1800",
-          "CDN-Cache-Control": "public, s-maxage=1800",
-          "Vercel-CDN-Cache-Control": "public, s-maxage=3600",
-        },
-      }
-    );
+    cache.set(cacheKey, data);
+
+    return new Response(JSON.stringify(data), {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Cache-Control": "public, s-maxage=1800",
+        "CDN-Cache-Control": "public, s-maxage=1800",
+        "Vercel-CDN-Cache-Control": "public, s-maxage=3600",
+      },
+    });
   } catch (error) {
     console.error(error);
+    return new Response("Error fetching data", { status: 500 });
   }
 }
