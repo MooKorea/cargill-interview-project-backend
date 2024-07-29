def5f57dd1f412b1d2878fdb12b5f2d0d6173686
MooKorea <69235304+MooKorea@users.noreply.github.com>
2024-07-29 18:37:27 -0500

    update cache

--- src/app/api/ticker-symbol/route.ts
+++ src/app/api/ticker-symbol/route.ts
@@ -9,22 +9,30 @@ export async function GET(request: Request) {
   const url = `https://financialmodelingprep.com/api/v3/search?query=${query.toUpperCase()}&limit=10&apikey=${
     process.env.FINANCIAL_MODELING_PREP_KEY
   }`;
-  const res = await fetch(url, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  const data = await res.json();
 
-  return Response.json(
-    { data },
-    {
-      status: 200,
+  try {
+    const res = await fetch(url, {
       headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Content-Type": "application/json",
       },
-    }
-  );
+    });
+    const data = await res.json();
+
+    return Response.json(
+      { data },
+      {
+        status: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+          "Cache-Control": "public, s-maxage=1800",
+          "CDN-Cache-Control": "public, s-maxage=1800",
+          "Vercel-CDN-Cache-Control": "public, s-maxage=3600",
+        },
+      }
+    );
+  } catch (error) {
+    console.error(error);
+  }
 }
